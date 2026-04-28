ccffc4f7ff8126ba1b6a2dc7431db898006fefe8
qwww-12 <laro1laro34@gmail.com>
2026-04-28 01:52:05 +0100

    update

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -35,23 +35,24 @@ AForm	*Intern::Obj_Shrubbery( const std::string &target ) const{
 	return new ShrubberyCreationForm(target); 
 }
 
-AForm	*makeForm( const std::string &nform, const std::string &tform )
+AForm	*Intern::makeForm( const std::string &nform, const std::string &tform )
 {
 	std::string	all_forms[3] = {
 		"robotomy request", 
 		"presidential pardon", 
 		"shrubbery creation"
 	};
-	AForm	(Intern::*ptr[3])( const std::string &target ) ={
-		&Intern::Obj_Robot( const std::string &target ),
-		&Intern::Obj_Pre( const std::string &target ),
-		&Intern::Obj_Shrubbery( const std::string &target )
+	AForm	*(Intern::*ptr[3])( const std::string & ) const = {
+		&Intern::Obj_Robot,
+		&Intern::Obj_Pre,
+		&Intern::Obj_Shrubbery
 	};
 	for (int idx = 0; idx < 1; idx++){
 		if (all_forms[idx] == nform){
 			std::cout << "Intern creates " << nform << std::endl;
-			return (*ptr[idx])(tform);
+			return (this->*ptr[idx])(tform);
 		}
 	}
 	std::cout << "Intern fails creates " << nform << std::endl; // edit this printing by throwing and catching
+	return NULL;
 }
