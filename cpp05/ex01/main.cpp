08b2d63418971e59321e91d3f57f1d115064c88d
qwww-12 <laro1laro34@gmail.com>
2026-05-01 21:32:34 +0100

    update

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -3,31 +3,35 @@
 int main()
 {
 	try{
-		Bureaucrat b5("rr", 2);
-		Bureaucrat b1("Sif", 50);
-		Bureaucrat b2("chunk", 50);
-		Bureaucrat b3("abc", 0);
-		Bureaucrat b4("d", -1);
+		Bureaucrat b("B", 2);
+		Form	f("fo", -89, 13);
 	} catch (std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
+	
 	try{
-		Form f("Bob", 40, 50);
-		Form f2("Ssl", 89, 78);
-		Form f4("Rt", 5, 12);
-		Form f3("cb", -1, 12);
-	} catch( std::exception &e ){
+		Bureaucrat b("B", 151);
+		Form	f("fo", 45, 12);
+	} catch (std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
 
+	Bureaucrat 	b1("oL", 89);
+	Form		f1("Formmq", 88, 12);
+	b1.signForm(f1);
 	try{
-		Form f4("gh", 149, 151);
+		b1.BureaucratInc();
+		b1.BureaucratDec();
 	} catch (std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
-	b1.signForm(f);
-	b2.signForm(f2);
-	b5.signForm(f2);
-	b1.signForm(f2);
-	b5.signForm(f4);
+	b1.signForm(f1);
+	Bureaucrat	b2("L", 150);
+	try{
+		b2.BureaucratDec();
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
 }
