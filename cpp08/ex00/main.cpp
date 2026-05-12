639c29b32819192c139b18fc1068abea701cb5e1
qwww-12 <laro1laro34@gmail.com>
2026-05-12 16:23:58 +0100

    update

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -3,21 +3,33 @@
 int main( void )
 {
     try{
-        std::vector<int> vec = {12, 23, 89, 90};
+        std::vector<int> vec;
+        vec.push_back(12);
+        vec.push_back(23);
+        vec.push_back(89);
+        vec.push_back(90);
         easyfind(vec, 45);
     } catch(const std::exception &e){
         std::cout << e.what() << std::endl;
     }
 
     try{
-        std::array<int, 5> arr = {12, 89, 9, -9, 99};
-        easyfind(arr, 12);
+        std::vector<int> vec;
+        vec.push_back(12);
+        vec.push_back(89);
+        vec.push_back(9);
+        vec.push_back(-9);
+        vec.push_back(99);
+        easyfind(vec, 9);
     } catch(const std::exception &e){
         std::cout << e.what() << std::endl;
     }
 
     try{
-        std::list<int> lkd = {89, 90, 999};
+        std::list<int> lkd;
+        lkd.push_back(89);
+        lkd.push_back(90);
+        lkd.push_back(999);
         easyfind(lkd, 999);
     } catch(const std::exception &e){
         std::cout << e.what() << std::endl;
